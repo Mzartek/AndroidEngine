c67418633c0bab9e00aa263e519cecc544b9c46e
Havranek Kevin <mzartek@hotmail.fr>
2015-11-02 18:44:02 +0100

    Tiny change

--- app/src/main/java/com/paris8/univ/androidproject/GameActivity.java
+++ app/src/main/java/com/paris8/univ/androidproject/GameActivity.java
@@ -8,9 +8,9 @@ import com.paris8.univ.androidproject.engine.EngineView;
 
 import java.io.File;
 
-public class MainActivity extends Activity
+public class GameActivity extends Activity
 {
-    final static private String TAG = "MainActivity";
+    final static private String TAG = "GameActivity";
 
     GLSurfaceView mView;
 
@@ -31,35 +31,10 @@ public class MainActivity extends Activity
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         mView.onResume();
     }
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 
     static
     {
