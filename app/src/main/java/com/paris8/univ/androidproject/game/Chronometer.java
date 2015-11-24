6fed34e1f01d68733ea6c1ca57a254a932e4c877
Havranek Kevin <mzartek@hotmail.fr>
2015-11-24 13:04:07 +0100

    Add a chronometer

---
+++ app/src/main/java/com/paris8/univ/androidproject/game/Chronometer.java
@@ -0,0 +1,37 @@
+package com.paris8.univ.androidproject.game;
+
+/**
+ * Created by Mzartek on 24/11/15.
+ */
+public final class Chronometer
+{
+    private static long begin, end;
+
+    public static void start(){
+        begin = System.currentTimeMillis();
+    }
+
+    public static void stop(){
+        end = System.currentTimeMillis();
+    }
+
+    public static long getTime() {
+        return end-begin;
+    }
+
+    public static long getMilliseconds() {
+        return end-begin;
+    }
+
+    public static double getSeconds() {
+        return (end - begin) / 1000.0;
+    }
+
+    public static double getMinutes() {
+        return (end - begin) / 60000.0;
+    }
+
+    public static double getHours() {
+        return (end - begin) / 3600000.0;
+    }
+}
